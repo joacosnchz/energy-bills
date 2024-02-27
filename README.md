607c418da15816badc9b552448a2d6dcd243d691
Joaquin Sanchez <js@bluelight.com.ar>
2024-02-27 11:09:09 -0300

    Add quality checks to customers load

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ This project obtains data form Emporia Energy API (unofficial) to generate invoi
 - [Google sheets API](https://developers.google.com/sheets/api/quickstart/python)
 - [Stripe API](https://docs.stripe.com/api)
 
-# Build and run
-- To start local services: `cd deployments/local && docker compose up -d`
+# Setup local environment
+1. Create `.env` file based on `.env.example` on `deployments/local` and on `migrations/`
+2. Start local services: `cd deployments/local && docker compose up -d`
+3. Run migrations: `cd migrations && ./upgrade.sh`
+
+# Run
 - To build: `deployments/local/build.sh`
 - To run: `deployments/local/run.sh`
--- src/energy_bills/controllers/customers.py
+++ src/energy_bills/controllers/customers.py
@@ -21,12 +21,16 @@ class Customers:
             )
 
             df = pd.DataFrame(data)
+            cls.validate_number_of_columns(df)
             df = df.drop(0)
             df.columns = [
                 "pad_id", "aniversary_day", "first_name", "last_name", "phone", "email", "move_in", "move_out_day",
                 "devices"
             ]
             df["property_owner_id"] = po.id
+            df = cls.filter_invalid_rows(df)
+            df = cls.replace_empty_strings(df)
+            df = cls.change_data_types(df)
 
             logger.info(f"Found {len(df)} valid rows")
 
@@ -34,3 +38,52 @@ class Customers:
                 Customer.upsert(row.to_dict())
 
         logger.info("Customers load function executed")
+
+    @classmethod
+    def filter_invalid_rows(cls, df: DataFrame) -> DataFrame:
+        # Filter nulls
+        not_null_columns = ["pad_id", "aniversary_day", "first_name", "last_name", "email", "devices"]
+        df = df[~df[not_null_columns].isnull().any(axis=1)]
+
+        # Filter invalid types
+        int_columns = ["pad_id", "aniversary_day"]
+        for column in int_columns:
+            df = df[df[column].apply(lambda x: x.isnumeric())]
+
+        # Filter invalid nullable types
+        nullable_int_columns = ["move_out_day"]
+        for column in nullable_int_columns:
+            df = df[df[column].apply(lambda x: not x or x.isnumeric())]
+
+        # Validates devices column
+        pattern = r"^([0-9](,)?)+$"
+        df["devices_valid"] = df["devices"].str.contains(pattern, regex=True)
+        df = df[df["devices_valid"]]
+        df = df.drop(columns=["devices_valid"])
+
+        return df
+
+    @classmethod
+    def replace_empty_strings(cls, df: DataFrame) -> DataFrame:
+        df["move_in"] = df["move_in"].replace("", None)
+        df["move_out_day"] = df["move_out_day"].replace("", None)
+        df["phone"] = df["phone"].replace("", None)
+        df["devices"] = df["devices"].replace("", None)
+
+        return df
+
+    @classmethod
+    def change_data_types(cls, df: DataFrame) -> DataFrame:
+        df["pad_id"] = df["pad_id"].astype(int)
+        df["aniversary_day"] = df["aniversary_day"].astype(int)
+
+        df["move_out_day"] = df["move_out_day"].astype('Int64')
+
+        return df
+
+    @classmethod
+    def validate_number_of_columns(cls, df: DataFrame) -> None:
+        expected_col_number = 9
+        if df.shape[1] != expected_col_number:
+            logger.error(f"Number of columns is not {expected_col_number}")
+            raise Exception(f"Number of columns is not {expected_col_number}")
