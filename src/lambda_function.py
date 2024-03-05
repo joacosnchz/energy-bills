de500fd6b1f327c5f53796c16a8765852a1f7c4c
Joaquin Sanchez <js@bluelight.com.ar>
2024-03-05 13:48:21 -0300

    Lambda function initial tests

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,22 @@
+import logging
+import os
+
+from energy_bills.controllers.customers import Customers
+from energy_bills.controllers.usages import Usages
+
+logging.basicConfig(level=logging.INFO)
+
 
 def lambda_handler(event, context):
+    job = os.getenv("JOB")
+
+    if job == "invoices":
+        Usages.load()
+    elif job == "customers":
+        Customers.load()
+    else:
+        logging.error("Job does not exist")
+
     return {
         "statusCode": 200,
         "body": "OK"
